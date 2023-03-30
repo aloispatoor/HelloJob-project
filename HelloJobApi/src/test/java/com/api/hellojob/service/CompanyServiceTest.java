343834798f55a653606f0ca21f0d0197443b72e6
Alois Patoor <palois89@posteo.net>
2023-03-30 15:35:01 +0200

    Company Service Test OK

---
+++ HelloJobApi/src/test/java/com/api/hellojob/service/CompanyServiceTest.java
@@ -0,0 +1,43 @@
+package com.api.hellojob.service;
+
+import com.api.hellojob.entity.Company;
+import com.api.hellojob.repository.CompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class CompanyServiceTest {
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Test
+    public void save_NominalCase(){
+        Company company = new Company();
+        company.setEmail("test@test.com");
+        company.setName("Test Company");
+        company.setLocation("Test City");
+        companyRepository.save(company);
+        System.out.println("Test save end");
+    }
+
+    @Test
+    public void find_NominalCase(){
+        Company company = companyRepository.findById(1L).get();
+        assertThat(company.getName()).as("Company not found").isEqualTo("Test Company");
+    }
+
+    @Test
+    public void find_allCases(){
+        List<Company> companies = companyRepository.findAll();
+        assertThat(companies).as("Not all companies were found").hasSize(1);
+        companies.forEach(System.out::println);
+    }
+}
