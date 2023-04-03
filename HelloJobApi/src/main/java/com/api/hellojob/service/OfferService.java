6c5b3a66120f66624d0a43b1180b23b6badd2279
Alois Patoor <palois89@posteo.net>
2023-04-03 15:02:37 +0200

    starting OfferService

---
+++ HelloJobApi/src/main/java/com/api/hellojob/service/OfferService.java
@@ -0,0 +1,43 @@
+package com.api.hellojob.service;
+
+import com.api.hellojob.entity.Company;
+import com.api.hellojob.entity.Offer;
+import com.api.hellojob.repository.CompanyRepository;
+import com.api.hellojob.repository.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OfferService {
+    @Autowired
+    private OfferRepository offerRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public List<Offer> getAllOffers(){
+        return offerRepository.findAll();
+    }
+
+    public Optional<Offer> getOfferById(Long id){
+        return offerRepository.findById(id);
+    }
+
+    public Offer addOffer(Offer offer, Long id){
+        Company company = companyRepository.findById(id).get();
+        offer.setAuthor(company);
+        return offerRepository.save(offer);
+    }
+
+    public void deleteOffer(Long id){
+        Offer offer = offerRepository.findById(id).orElse(null);
+        Company currentCompany = new Company();
+        Company company = offer.getAuthor();
+        if(currentCompany == company){
+            offerRepository.deleteById(id);
+            System.out.println("Offer " + id + " deleted successfully");
+        }
+    }
+}
